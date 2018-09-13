7fcac237939f7dd0b7ae2a80a70f30f19baaa9c2
Alex Teterin <at28813905@gmail.com>
2018-09-13 17:36:06 +0300

    исправлена lab1_4

--- lab1/Lab14.js
+++ lab1/Lab14.js
@@ -1,19 +1,22 @@
 function Calculator() {
     this.read = function() {
-        this.firstOperand = prompt('Enter fitst operand', 0);
-        this.secondOperand = prompt('Enter second operand', 0);
-    } 
+        this.firstOperand = +prompt('Enter fitst operand', 0);
+        this.secondOperand = Number(prompt('Enter second operand', 0));
+    }
+
+    console.log(this.firstOperand);
+
     this.sum = function() {
-        return Number(this.firstOperand) + Number(this.secondOperand);
+        return this.firstOperand + this.secondOperand;
     }
 
     this.mul = function() {
-        return Number(this.firstOperand) * Number(this.secondOperand);
+        return this.firstOperand * this.secondOperand;
     }
 }
 
 var calculator = new Calculator();
 calculator.read();
 
-alert("Сумма=" +  calculator.sum());
-alert("Произведение=" +  calculator.mul());
+alert(isNaN(calculator.sum()) ? "одно или оба введенных значения не являются числом" : "Сумма = " + calculator.sum());
+alert(isNaN(calculator.mul()) ? "одно или оба введенных значения не являются числом" : "Произведение = " + calculator.mul());
